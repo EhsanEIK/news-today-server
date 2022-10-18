ce6963db265a84bee4e3926a84d4d8bfcfceeb64
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-18 09:48:28 +0600

    added CORS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
 
 const categoris = require('./data/categories.json');
 
