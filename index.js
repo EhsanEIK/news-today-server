3a9cab18a709b6070a380cab3ba54422eb4fd4c7
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-20 09:06:25 +0600

    created singleNews api to show specific news

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/news', (req, res) => {
     res.send(news);
 })
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const singleNews = news.find(n => n._id === id);
+    res.send(singleNews);
+})
+
 app.listen(port, () => {
     console.log("News server is runnin on port:", port);
 })
